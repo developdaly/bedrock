304f6a08e7341a94ea5663f6e4e2617b901dec08
Patrick Daly <patrick@developdaly.com>
2013-12-28 09:22:11 -0600

    Set auth keys and salts

--- config/application.php
+++ config/application.php
@@ -37,14 +37,14 @@ $table_prefix = 'wp_';
  * Authentication Unique Keys and Salts
  * https://api.wordpress.org/secret-key/1.1/salt
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '}a 85hHk%!Olo%W}D>_z|w]uEQ>Cye6U|iLIa`|#Y3PooS|9K{Eof;1u[ei:%n2k');
+define('SECURE_AUTH_KEY',  'r;l~A7-E[IsoV+Q8g(.IzCy|2McIa@A<kQWc9$7<[)l$b@8W*+iP.wzb(|ixrW%}');
+define('LOGGED_IN_KEY',    '-d~{5;zhw]4ClaQH>iC2!y}?0o7|#%AOV+RSIH5;Hf5O;@W]Y6!Uk]CXmCUVCNL,');
+define('NONCE_KEY',        'U]fd|M]P1BqEQh]<cImh1[uKhPql4ZO6z_9W)G|@cHPnXQ!Tj0UP,OkK4sLd12@/');
+define('AUTH_SALT',        '&%q]3-kv+a4m2HdOnGzB|PFG}|v:|-n}5ct/hXw+r%>LFF-H-E=kn382(|;HF0Oo');
+define('SECURE_AUTH_SALT', '%OMH%~bB`Y<S~d/&>Y@J),pl- ^q_W>7X[F<G7js-3S()rHc_7p_TF,NeC]m^lb8');
+define('LOGGED_IN_SALT',   'A*l[N5`S315#~`3C[dj/h@F(T}z4Pk||~@iXvh7hu9~46[8L4,e!_>-a5o.)=wK,');
+define('NONCE_SALT',       'i[-LZ9xpf#6AE}h1^u4e #x#+CU<IHL w:5U]qzowoll(f&`RgTT7y<1+/!hW]]r');
 
 /**
  * Custom Settings
